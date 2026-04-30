5ceb725e4a95d86867f21c393582ea038ecd2032
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-04-29 21:07:05 -0400

    Varibales

---
+++ intro/variable.js
@@ -0,0 +1,6 @@
+let greetings = "Hello Neel";
+// greetings = 6
+let myNum = 6;
+// myNum.toUpperCase();
+console.log(greetings);
+export {};
---
+++ intro/variable.ts
@@ -0,0 +1,10 @@
+let greetings:string = "Hello Neel"
+
+// greetings = 6
+let myNum:number = 6
+
+// myNum.toUpperCase();
+console.log(greetings)
+
+
+export{}
