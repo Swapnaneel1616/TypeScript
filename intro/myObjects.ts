cbb4f5ab99b53a4a8ed8c273f29e61e75732e397
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-04-30 11:06:11 -0400

    Type User

--- intro/myObjects.ts
+++ intro/myObjects.ts
@@ -19,6 +19,18 @@ function createCourse():{name:string , price:number}{
 }
 
 
+type User = {
+    name : string;
+    email: string;
+    isActive: boolean;
+}
+
+function createUsers(user: User):User{
+    return {name: user.name, email: user.email, isActive: user.isActive}
+}
+
+createUsers({name:"neel", email: "neel@example.com", isActive: true})
+
 
 
 
