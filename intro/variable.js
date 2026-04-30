c9383578f79eaad3ab42ee79be5af452b8755d76
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-04-29 21:26:55 -0400

    function varibale

--- intro/variable.js
+++ intro/variable.js
@@ -1,6 +1,13 @@
 let greetings = "Hello Neel";
 // greetings = 6
 let myNum = 6;
+let isLogged = true;
 // myNum.toUpperCase();
 console.log(greetings);
+let hero;
+function getHero() {
+    // return 43
+    return "Batman";
+}
+hero = getHero();
 export {};
--- intro/variable.ts
+++ intro/variable.ts
@@ -3,8 +3,22 @@ let greetings:string = "Hello Neel"
 // greetings = 6
 let myNum:number = 6
 
+
+let isLogged: boolean = true
+
 // myNum.toUpperCase();
 console.log(greetings)
+
+
+
+let hero: string
+
+function getHero(){
+    // return 43
+    return "Batman"
+}
+
+hero = getHero()
 
 
 export{}
