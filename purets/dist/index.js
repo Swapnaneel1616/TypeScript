a9a3a57f5151a8043168674d77a542bfadca0224
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-05-03 20:01:02 -0400

    class , Access Specifier

--- purets/dist/index.js
+++ purets/dist/index.js
@@ -1,3 +1,24 @@
-console.log("Hello Typescript Check ");
+// class User{
+//     email:string
+//     name:string
+//     readonly city: string = "kolkata"
+//     constructor(email:string , name:string){
+//         this.email = email
+//         this.name = name
+//     }
+// }
+class User {
+    email;
+    name;
+    city = "kolkata";
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+}
+const neel = new User("h@h.com", "neel");
+console.log(neel.email);
+console.log(neel.name);
+console.log(neel.city);
 export {};
 //# sourceMappingURL=index.js.map
--- purets/src/index.ts
+++ purets/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello Typescript Check ")
+// class User{
+//     email:string
+//     name:string
+//     readonly city: string = "kolkata"
+//     constructor(email:string , name:string){
+//         this.email = email
+//         this.name = name
+//     }
+// }
+class User{
+    readonly city: string = "kolkata"
+    constructor(
+        public email:string ,
+        public name:string){
+    }
+}
+
+
+const neel = new User("h@h.com" ,"neel")
+
+console.log(neel.email)
+console.log(neel.name)
+console.log(neel.city)
