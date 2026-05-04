7d5ce4ff6cd27dfe42c5c27d67583559b4ad0d31
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-05-03 20:27:31 -0400

    GETTER SETTER

--- purets/dist/index.js
+++ purets/dist/index.js
@@ -10,10 +10,23 @@
 class User {
     email;
     name;
+    _coureCount = 1;
     city = "kolkata";
     constructor(email, name) {
         this.email = email;
         this.name = name;
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._coureCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._coureCount = courseNum;
     }
 }
 const neel = new User("h@h.com", "neel");
--- purets/src/index.ts
+++ purets/src/index.ts
@@ -8,11 +8,30 @@
 //     }
 // }
 class User{
+
+    private _coureCount = 1
+
+
     readonly city: string = "kolkata"
     constructor(
         public email:string ,
         public name:string){
     }
+
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+
+    get courseCount():number{
+        return this._coureCount
+    }
+    set courseCount(courseNum){
+        if(courseNum<=1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._coureCount = courseNum
+    }
+
 }
 
 
